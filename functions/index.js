d3f9928626843f0d8d6fd9a9342faeccd7cc2a42
Anh Triet <anhtriet1810@gmail.com>
2025-06-27 00:15:46 -0500

    Backend kinda thing

--- functions/index.js
+++ functions/index.js
@@ -3,7 +3,6 @@ const admin = require("firebase-admin");
 
 admin.initializeApp();
 
-// Simple HTTP function to test deployment
 exports.ping = functions.https.onRequest((req, res) => {
-  res.status(200).send("Ping received! 🎯 Function is working.");
+  res.send("Ping OK");
 });
